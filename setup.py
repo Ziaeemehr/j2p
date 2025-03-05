8ec6ab3aa73d7b445ac4252029849712a9d85f06
Abolfazl  Ziaeemehr <a.ziaeemehr@gmail.com>
2025-03-05 14:48:26 +0100

    Update package name to 'ju2py' and add twine to requirements; specify repository URL in publish workflow

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="j2p",
+    name="ju2py",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["nbformat", "nbconvert"],
+    install_requires=["nbformat", "nbconvert", "twine"],
     entry_points={
         "console_scripts": [
             "j2p=j2p.cli:main",
         ],
     },
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Abolfazl Ziaeemehr",
+    author_email="a.ziaeemehr@gmail.com",
     description="A simple tool to convert Jupyter notebooks to Python scripts.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/j2p",
+    url="https://github.com/ziaeemehr/ju2py",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
